1a520f5685f511f0d288549df719b28179be6858
Axel <axeldiaz10@gmail.com>
2020-07-13 14:03:30 -0300

    [#4] Add clients views

---
+++ src/views/clients/form/FormClientView.styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+`
+
+const Avatar = styled.div`
+    align-self: center;
+    margin: 10px 0px;
+`
+
+export {
+    Container,
+    Avatar
+}
